2045d504064c5629b4290277c1d26d71f9d96d7c
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-21 13:09:03 +0100

    setup: Install chakra-ui and test setup by rendering a Button component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import { Button } from '@chakra-ui/button';
 
 function App() {
   return (
     <div className="App">
+      <Button colorScheme="blue">Button</Button>
     </div>
   );
 }
