73026b7f8615a5ab8f19312eb8422eac11d99d5b
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-21 15:34:14 +0100

    refactor: Update import paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { Route } from 'react-router-dom';
-import Home from './Home';
-import Chats from './Chats';
+import Home from './pages/HomePage';
+import ChatPage from './pages/ChatPage';
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={ Home } />
-      <Route path="/chats" component={ Chats } />
+      <Route path="/chats" component={ ChatPage } />
     </div>
   );
 }
