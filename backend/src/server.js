c6a8d2d8d5eee69d500be955be6cdfb6be426d00
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-21 14:29:55 +0100

    setup: Install cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const app = express();
-const chats = require("../data/data")
+const app = require("express")();
+const chats = require("../data/data");
+
+app.use(require("cors")());
 
 app.get("/api/chat", (req, res) => {
-    res.status(200).json(chats);
-})
+  res.status(200).json(chats);
+});
 
 app.get("/api/chat/:id", (req, res) => {
-    const chat = chats.find(chat => chat._id === req.params.id)
-    if (chat) {
-        res.status(200).json(chat);
-    } else {
-        res.status(404).json({ message: "Chat not found" });
-    }
+  const chat = chats.find((chat) => chat._id === req.params.id);
+  if (chat) {
+    res.status(200).json(chat);
+  } else {
+    res.status(404).json({ message: "Chat not found" });
+  }
 });
 
 module.exports = app;
