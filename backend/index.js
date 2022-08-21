c41a880f59fbf8fbffefeab2b823190f40b74434
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-21 12:41:46 +0100

    setup: install dotenv, create .env & apply environment variables to API entry point (index.js)

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const app = require('./src/server.js');
+const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
