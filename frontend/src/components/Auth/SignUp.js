cb2e169612e0543f418330608c8ef8c3e2d05b2f
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-23 11:20:35 +0100

    feature: SignUp component renders sign up form

--- frontend/src/components/Auth/SignUp.js
+++ frontend/src/components/Auth/SignUp.js
@@ -1,7 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
 
 export default function SignUp() {
+  const [isPasswordShowing, setisPasswordShowing] = useState(false);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [pic, setPic] = useState("");
+
+  function handleClick() {
+    setisPasswordShowing(!isPasswordShowing);
+  }
+
+  function postDetails() {}
+
+  function submitHandler() {}
+
   return (
-    <div>SignUp</div>
-  )
+    <VStack spacing="5px">
+      <FormControl id="first-name">
+        <FormLabel>Name:</FormLabel>
+        <Input
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Password:</FormLabel>
+        <InputGroup>
+          <Input
+            type={isPasswordShowing ? "text" : "password"}
+            placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {isPasswordShowing ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Confirm Password:</FormLabel>
+        <InputGroup>
+          <Input
+            type={isPasswordShowing ? "text" : "password"}
+            placeholder="Confirm your password"
+            isInvalid={password !== confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {isPasswordShowing ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Upload Picture:</FormLabel>
+        <Input
+          type="file"
+          p="1.5"
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
+        />
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Submit
+      </Button>
+    </VStack>
+  );
 }
